02c9a5b822ea64de7edfcc86e05bdc302e88735b
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-25 19:36:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ end
 def print_first_directors_movie_titles
   counter = 0 
   while counter < directors_database[0][:movies].length do
-    puts output directors_database[0][:titles]
+    puts directors_database[0][:movies][counter][:title]
     counter += 1
   end
 end
