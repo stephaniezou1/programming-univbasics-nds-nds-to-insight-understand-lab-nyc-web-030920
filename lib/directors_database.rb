776a101de41329acac2c4ae2ad6225d6b77af6b7
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-25 19:25:24 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,8 +1,6 @@
 require 'yaml'
-require 'pp'
 
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-  pp directors_db
 end
